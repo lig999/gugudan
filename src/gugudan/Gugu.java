c89807d5f93a0e473c322fffdca7348b86f2a282
lliigg <lliigg@localhost>
2021-05-08 12:55:14 +0900

    me gugu

--- src/gugudan/Gugu.java
+++ src/gugudan/Gugu.java
@@ -1,29 +1,35 @@
 package gugudan;
+import java.util.Scanner;
 
 public class Gugu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   
-   System.out.println("4단 ");
-		int result = 4*1;
-		  System.out.println(result);
-		   result = 4*2;
-		  System.out.println(result);
-		   result = 4*3;
-		  System.out.println(result);
-		  result = 4*4;
-		  System.out.println(result);
-		   result = 4*5;
-		  System.out.println(result);
-		   result = 4*6;
-		  System.out.println(result);
-		   result = 4*7;
-		  System.out.println(result);
-		   result = 4*8;
-		  System.out.println(result);
-		   result = 4*9;
-		  System.out.println(result);
+		System.out.println("구구단중 출력할 단은? ");
+		Scanner iss= new Scanner(System.in);
+       int a = iss.nextInt();
+	System.out.println("number :" + a);
+		
+	
+	int ressult = a*1;
+	System.out.println( ressult);
+	ressult = a*2;
+	System.out.println( ressult);
+	ressult = a*3;
+	System.out.println( ressult);
+	 ressult = a*4;
+	System.out.println( ressult);
+	ressult = a*5;
+	System.out.println( ressult);
+	ressult = a*6;
+	System.out.println( ressult);
+	 ressult = a*7;
+	System.out.println( ressult);
+	ressult = a*8;
+	System.out.println( ressult);
+	ressult = a*9;
+	System.out.println( ressult);
+	
 		  }
 
 }
