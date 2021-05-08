2a5b21727b6714ee8fd7758e3d1f2af34079c45d
lliigg <lliigg@localhost>
2021-05-08 12:15:58 +0900

    gugudan

---
+++ src/gugudan/Gugu.java
@@ -0,0 +1,13 @@
+package gugudan;
+
+public class Gugu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(2*1);
+		Systme.out.print
+		
+	}
+
+}
---
+++ src/gugudan/Gugudan_1.java
@@ -0,0 +1,10 @@
+package gugudan;
+
+public class Gugudan_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
