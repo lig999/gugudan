22ac4d9e6f0cad5b0577903311c3709ef095a3c1
lliigg <lliigg@localhost>
2021-05-08 12:37:58 +0900

    cmd

--- src/gugudan/Gugu.java
+++ src/gugudan/Gugu.java
@@ -4,10 +4,26 @@ public class Gugu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println(2*1);
-		System.out.print(2*1);
-		
-	}
+   
+   System.out.println("4단 ");
+		int result = 4*1;
+		  System.out.println(result);
+		   result = 4*2;
+		  System.out.println(result);
+		   result = 4*3;
+		  System.out.println(result);
+		  result = 4*4;
+		  System.out.println(result);
+		   result = 4*5;
+		  System.out.println(result);
+		   result = 4*6;
+		  System.out.println(result);
+		   result = 4*7;
+		  System.out.println(result);
+		   result = 4*8;
+		  System.out.println(result);
+		   result = 4*9;
+		  System.out.println(result);
+		  }
 
 }
